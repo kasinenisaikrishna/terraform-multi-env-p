c98801ac49728145f89f37596271d5a5fce90dd0
kasinenisaikrishna <kasineni.sai@gmail.com>
2025-12-04 20:45:23 +0530

    terraform

---
+++ tfvars/variables.tf
@@ -0,0 +1,16 @@
+variable "instances" {
+  type = map
+  default = {
+    mysql    = "t3.small"
+    backend  = "t3.micro"
+    frontend = "t3.micro"
+  }
+}
+
+variable "zone_id" {
+  default = "Z033712422B61CO3J7OXS"
+}
+
+variable "domain_name" {
+  default = "dawsconnect.org"
+}
